6072c8b741b2a93724405949a1c89da2d9029f33
software-eclouds-energy-llp <software@ecloudsenergy.com>
2026-07-22 12:06:01 +0530

    First push

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXJ84sB7BLjpYmlwmLZDc7Z1y2jQl1cwE",
+  authDomain: "kt-quiz-e7858.firebaseapp.com",
+  databaseURL: "https://kt-quiz-e7858-default-rtdb.firebaseio.com",
+  projectId: "kt-quiz-e7858",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
